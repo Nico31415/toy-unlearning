f59c77fe50c4ba86ae4e1ce1022b78b78c7155ce
Nicolas Anguita <na658@login-q-3.data.cluster>
2025-09-21 15:39:53 +0100

    debugging sparse overlap

--- experiments/diagonal/diagonal_sparse_overlap_1.py
+++ experiments/diagonal/diagonal_sparse_overlap_1.py
@@ -7,7 +7,7 @@ from functions.array_training import ArgparseArray, name_instance
 
 c_init = 10**-5
 scaling_init = 1e-3
-# lmdas_init = [0, -0.00001]  # [0, -1e-5] to match pretraining
+lmdas_init = [0, -0.00001]  # [0, -1e-5] to match pretraining
 lmdas_init = [0]
 
 
@@ -15,9 +15,9 @@ argparse_array = ArgparseArray(
     seed=[0],  # Fixed seed to match pretraining
     active_dim_1=40,
     active_dim_2=[5, 40],  # Test both small and large finetuning tasks
-    scaling = [1.0],
+    scaling = [1e-3],
     # scaling=[1e-3, 1.0],
-    model_scaling=scaling_init,
+    model_scaling=[1e-2, 1.0],
     inp_dim=1000,
     # Map to the correct pretrained model based on init_method and lmda
     model_path=(lambda init_method, seed, lmda, c, **kwargs: f'data/diagonal/pretrain/seed={seed}--active_dim=40--c={c}--scaling={scaling_init}--lmda={lmda}--init_method={init_method}/model.pt'),
@@ -27,9 +27,11 @@ argparse_array = ArgparseArray(
     one_task=[True],
     linear_readout=(lambda linear_readout, **kwargs: linear_readout),
     n_train1=1024,
-    n_train2 = [2** i for i in range(4, 10)],
+    #n_train2 = [2** i for i in range(4, 10)],
+    n_train2 = [64],
     # n_train2=64,  # Fixed training size for this experiment
-    aux_overlap_bool=['yes', 'no'],  # Test both overlap and no overlap
+    aux_overlap_bool =['yes'],
+    #aux_overlap_bool=['yes', 'no'],  # Test both overlap and no overlap
     overlap=(lambda overlap_bool, active_dim_2, **kwargs: 0 if overlap_bool=='no' else active_dim_2),
     lr=1e-1,
     lmda=(lambda lmda, **kwargs: f"{lmda:.10f}"),
@@ -43,8 +45,8 @@ argparse_array = ArgparseArray(
     aux_load_model=[True],
     aux_linear_readout=[False],
     # Use name_instance for cleaner save path
-    save_path=name_instance('init_method', 'seed', 'n_train2', 'active_dim_2', 'load_model', 'linear_readout', 'one_task', 'overlap_bool', 'lmda', 'c', 'scaling',
-                            base_folder='data/diagonal/sparse_overlap2'),
+    save_path=name_instance('init_method', 'seed', 'n_train2', 'active_dim_2', 'load_model', 'linear_readout', 'one_task', 'overlap_bool', 'lmda', 'c', 'model_scaling',
+                            base_folder='data/diagonal/sparse_overlap3'),
     save_weights=True
 )
 
